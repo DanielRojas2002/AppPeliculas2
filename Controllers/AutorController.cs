b217c6583415bc149212ad26486c52e6e2b623bd
DanielRojas2002 <danymessi1000@gmail.com>
2023-05-17 11:13:15 -0600

    CRUD AUTORES

---
+++ Controllers/AutorController.cs
@@ -0,0 +1,100 @@
+﻿using AppPeliculas.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppPeliculas.Controllers
+{
+    public class AutorController : Controller
+    {
+        private readonly DbpeliculasContext _context;
+
+        public AutorController(DbpeliculasContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Autors.ToList());
+        }
+
+        public IActionResult Upsert(int? idautor)
+        {
+
+            Autor autor = new Autor();
+            if (idautor == null)
+            {
+                // crear
+                autor.IdAutor = 0;
+                return View(autor);
+            }
+            else
+            {
+                // editar
+                autor = _context.Autors.Find(idautor);
+                return View(autor);
+            }
+            
+        }
+
+        [HttpPost]
+        public IActionResult Upsert(Autor modelo)
+        {
+            if (modelo.IdAutor == 0)
+            {
+                // crear
+
+                if (ModelState.IsValid)
+                {
+                    Autor autor = new Autor()
+                    {
+                        Nombre = modelo.Nombre.ToUpper(),
+                        APaterno = modelo.APaterno.ToUpper(),
+                        AMaterno = modelo.AMaterno.ToUpper()
+
+                    };
+
+                    _context.Autors.Add(autor);
+                    _context.SaveChanges();
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    return View(modelo);
+                }
+
+            }
+            else
+            {
+                // editar
+                Autor autor = new Autor()
+                {
+                    IdAutor=modelo.IdAutor,
+                    Nombre = modelo.Nombre.ToUpper(),
+                    APaterno = modelo.APaterno.ToUpper(),
+                    AMaterno = modelo.AMaterno.ToUpper()
+
+                };
+                _context.Autors.Update(autor);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+          
+        }
+
+        public IActionResult Eliminar(int? idautor)
+        {
+            return View(_context.Autors.Find(idautor));
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Eliminar (Autor modelo)
+        {
+            _context.Autors.Remove(modelo);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+
+        }
+    }
+}
