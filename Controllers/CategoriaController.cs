049f5039cfdc5c5cbb039ed1d851d9a6db7f2eac
DanielRojas2002 <danymessi1000@gmail.com>
2023-05-17 10:42:46 -0600

    Cambio CRUD CATEGORIAS

---
+++ Controllers/CategoriaController.cs
@@ -0,0 +1,102 @@
+﻿using AppPeliculas.Models;
+using Microsoft.AspNetCore.Mvc;
+using AppPeliculas.Models;
+namespace AppPeliculas.Controllers
+{
+    public class CategoriaController : Controller
+    {
+
+
+        private readonly DbpeliculasContext _context;
+
+        public CategoriaController(DbpeliculasContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Categoria.ToList());
+        }
+
+
+        public IActionResult Upsert(int? idcategoria)
+        {
+           
+            Categorium categoria = new Categorium();
+            if (idcategoria == null)
+            {
+                categoria.IdCategoria = 0;
+               
+                return View(categoria);
+            }
+            else
+            {
+                categoria = _context.Categoria.Find(idcategoria);
+               
+                return View(categoria);
+            }
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Categorium modelo)
+        {
+            if (modelo.IdCategoria == 0)
+            {
+                if (ModelState.IsValid)
+                {
+                    Categorium categoria = new Categorium()
+                    {
+                        Descripcion = modelo.Descripcion.ToUpper()
+                    };
+                    _context.Categoria.Add(categoria);
+                    _context.SaveChanges();
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    return View(modelo);
+
+                }
+            }
+            else
+            {
+               
+
+                Categorium categoria = new Categorium()
+                {
+                    IdCategoria=modelo.IdCategoria,
+                    Descripcion = modelo.Descripcion.ToUpper()
+                };
+
+                _context.Categoria.Update(categoria);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+
+        }
+
+        public IActionResult Editar(int idcategoria)
+        {
+            
+            return View(_context.Categoria.Find(idcategoria));
+        }
+
+        public IActionResult Eliminar(int? idcategoria)
+        {
+
+            return View(_context.Categoria.Find(idcategoria));
+        }
+
+        [HttpPost]
+        public IActionResult Eliminar(Categorium modelo)
+        {
+            _context.Categoria.Remove(modelo);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
