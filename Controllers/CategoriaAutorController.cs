061d381a1fc0b8c90b7c5c98ffc57e7de42d91f0
DanielRojas2002 <danymessi1000@gmail.com>
2023-05-22 10:00:56 -0600

    CAMBIO

---
+++ Controllers/CategoriaAutorController.cs
@@ -0,0 +1,47 @@
+﻿using AppPeliculas.Models;
+using AppPeliculas.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppPeliculas.Controllers
+{
+    public class CategoriaAutorController : Controller
+    {
+
+        private readonly DbpeliculasContext _context;
+
+        public CategoriaAutorController (DbpeliculasContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+
+            // include debes de tener antes un modelo que junta las tablas 
+            var x = _context.CategoriaAutors.Include(m => m.IdCategoriaNavigation );
+
+
+            // aqui no hay 
+            var query_join4 = from tablaunion in _context.CategoriaAutors
+                              join Categoria in _context.Categoria
+                              on tablaunion.IdCategoria equals Categoria.IdCategoria
+                              join Autor in _context.Autors
+                              on tablaunion.IdAutor equals Autor.IdAutor
+                              select new CategoriaAutorVM
+                              {
+                                  idcategoria = tablaunion.IdCategoria,
+                                  descripcioncategoria = Categoria.Descripcion,
+                                  idautor = tablaunion.IdAutor,
+                                  nombrecompleto = Autor.Nombre + " " + Autor.APaterno + " " + Autor.AMaterno,
+
+
+                              };
+
+
+
+            return View(query_join4.ToList());
+        }
+    }
+}
---
+++ Models/ViewModels/CategoriaAutorVM.cs
@@ -0,0 +1,19 @@
+﻿namespace AppPeliculas.Models.ViewModels
+{
+    public class CategoriaAutorVM
+    {
+        public int? idcategoria { get; set; }
+
+        public int? idautor { get; set; }
+
+        public string? descripcioncategoria { get; set; }
+
+        public string? nombrecompleto { get; set; }
+
+
+
+
+
+
+    }
+}
